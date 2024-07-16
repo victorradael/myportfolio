16e2a2e8a663de81f26d154101d6f64db4ed9006
Victor Radael <victor.radael@hurb.com>
2024-07-16 19:17:05 -0300

    Update: Change Card Layout

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,39 +1,17 @@
-import React, { useState, useCallback, useEffect } from "react";
-import {
-  FaDocker,
-  FaAngular,
-  FaReact,
-  FaGithub,
-  FaLinkedin,
-  FaInstagram,
-  FaTwitter,
-  FaVuejs,
-  FaBootstrap,
-  FaAws,
-  FaGitAlt,
-  FaGitlab,
-  FaNodeJs,
-  FaJs,
-  FaHtml5,
-  FaCss3,
-  FaLinux,
-} from "react-icons/fa";
-import { DiPostgresql, DiMongodb, DiSass } from "react-icons/di";
+import React, { useState, useEffect } from "react";
+
+
 
 import api from "../../services/api";
+import Carousel from "../../components/Carousel"
 
 import {
   Front,
   Title,
-  DevInfoContacts,
   DevInfoHeader,
   View,
-  Back,
   ProfileImage,
-  Bio,
-  Email,
-  ArrowRight,
-  Techs,
+  Bio
 } from "./styles";
 
 interface IUser {
@@ -45,65 +23,7 @@ interface IUser {
 }
 
 const Card: React.FC = () => {
-  const [rotate, setRotate] = useState<boolean>(false);
   const [user, setUser] = useState<IUser>({} as IUser);
-
-  const techs = [
-    {
-      icon: <FaAngular color="#d6002f" size="3.75rem" />,
-    },
-    {
-      icon: <FaReact color="#5ed3f3" size="3.75rem" />,
-    },
-    {
-      icon: <FaReact color="#6550a0" size="3.75rem" />,
-    },
-    {
-      icon: <FaVuejs color="#00b37d" size="3.75rem" />,
-    },
-    {
-      icon: <FaBootstrap color="#613c8e" size="3.75rem" />,
-    },
-    {
-      icon: <FaAws color="#ff9900" size="3.75rem" />,
-    },
-    {
-      icon: <FaDocker color="#2391e6" size="3.75rem" />,
-    },
-    {
-      icon: <FaGitAlt color="#e94e31" size="3.75rem" />,
-    },
-    {
-      icon: <FaGithub color="#1b1f23" size="3.75rem" />,
-    },
-    {
-      icon: <FaGitlab color="#f46b25" size="3.75rem" />,
-    },
-    {
-      icon: <FaNodeJs color="#8bc500" size="3.75rem" />,
-    },
-    {
-      icon: <FaJs color="#efd81d" size="3.75rem" />,
-    },
-    {
-      icon: <FaHtml5 color="#f16529" size="3.75rem" />,
-    },
-    {
-      icon: <FaCss3 color="#00a8d9" size="3.75rem" />,
-    },
-    {
-      icon: <FaLinux color="#777" size="3.75rem" />,
-    },
-    {
-      icon: <DiPostgresql color="#316192" size="3.75rem" />,
-    },
-    {
-      icon: <DiMongodb color="#10a54e" size="3.75rem" />,
-    },
-    {
-      icon: <DiSass color="#c76494" size="3.75rem" />,
-    },
-  ];
 
   useEffect(() => {
     async function loadUser() {
@@ -114,64 +34,18 @@ const Card: React.FC = () => {
     loadUser();
   }, [user.login]);
 
-  const handleClick = useCallback(() => {
-    if (rotate) {
-      setRotate(false);
-    } else {
-      setRotate(true);
-    }
-  }, [rotate]);
   return (
     <>
-      <View rotate={rotate}>
-        <ArrowRight onClick={handleClick} />
-        <Front rotate={rotate}>
+      <View>
+       
+        <Front>
           <Title>{user.name}</Title>
           <DevInfoHeader>
             <ProfileImage src={user.avatar_url} alt="Profile" />
             <Bio>{user.bio}</Bio>
           </DevInfoHeader>
-
-          <DevInfoContacts>
-            <a
-              href="https://github.com/victorradael"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <FaGithub />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/victorradael/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://www.instagram.com/victorradael/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://twitter.com/VictorRadael"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <FaTwitter />
-            </a>
-          </DevInfoContacts>
-          <Email>victorradael@hotmail.com</Email>
+          <Carousel/>
         </Front>
-        <Back rotate={rotate}>
-          <Title>Knowledge</Title>
-          <Techs>
-            {techs.map((tech) => {
-              return <span>{tech.icon}</span>;
-            })}
-          </Techs>
-        </Back>
       </View>
     </>
   );
---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoscroll from 'embla-carousel-auto-scroll'
+import {Embla, EmblaContainer, EmblaSlide} from './styles'
+import {
+  FaDocker,
+  FaAngular,
+  FaReact,
+  FaGithub,
+  FaVuejs,
+  FaBootstrap,
+  FaAws,
+  FaGitAlt,
+  FaGitlab,
+  FaNodeJs,
+  FaJs,
+  FaHtml5,
+  FaCss3,
+  FaLinux,
+  FaPython,
+ 
+} from "react-icons/fa";
+import { DiPostgresql, DiMongodb, DiSass, DiGo, DiDjango, DiTerminal } from "react-icons/di";
+
+const Carousel: React.FC = () => {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoscroll({speed: 1})])
+
+  const techs = [
+    {
+      icon: <FaAngular color="#d6002f" size="3.75rem" />,
+    },
+    {
+      icon: <FaReact color="#5ed3f3" size="3.75rem" />,
+    },
+    {
+      icon: <FaReact color="#6550a0" size="3.75rem" />,
+    },
+    {
+      icon: <FaVuejs color="#00b37d" size="3.75rem" />,
+    },
+    {
+      icon: <FaBootstrap color="#613c8e" size="3.75rem" />,
+    },
+    {
+      icon: <FaAws color="#ff9900" size="3.75rem" />,
+    },
+    {
+      icon: <FaDocker color="#2391e6" size="3.75rem" />,
+    },
+    {
+      icon: <FaGitAlt color="#e94e31" size="3.75rem" />,
+    },
+    {
+      icon: <FaGithub color="#f1f1f1" size="3.75rem" />,
+    },
+    {
+      icon: <FaGitlab color="#f46b25" size="3.75rem" />,
+    },
+    {
+      icon: <FaNodeJs color="#8bc500" size="3.75rem" />,
+    },
+    {
+      icon: <FaJs color="#efd81d" size="3.75rem" />,
+    },
+    {
+      icon: <FaHtml5 color="#f16529" size="3.75rem" />,
+    },
+    {
+      icon: <FaCss3 color="#00a8d9" size="3.75rem" />,
+    },
+    {
+      icon: <FaLinux color="#777" size="3.75rem" />,
+    },
+    {
+      icon: <DiPostgresql color="#316192" size="3.75rem" />,
+    },
+    {
+      icon: <DiMongodb color="#10a54e" size="3.75rem" />,
+    },
+    {
+      icon: <DiSass color="#c76494" size="3.75rem" />,
+    },
+    {
+      icon: <DiGo color="#00a7d0" size="3.75rem" />,
+    },
+    {
+      icon: <DiDjango color="#0c4b33" size="3.75rem" />,
+    },
+    {
+      icon: <FaPython color="#326b9a" size="3.75rem" />,
+    },
+    {
+      icon: <DiTerminal color="#555" size="3.75rem" />,
+    },
+  ];
+
+  return (
+    <Embla ref={emblaRef}>
+      <EmblaContainer>
+         {techs.map((tech) => {
+              return <EmblaSlide>{tech.icon}</EmblaSlide>;
+            })}
+      </EmblaContainer>
+    </Embla>
+  )
+}
+export default Carousel
